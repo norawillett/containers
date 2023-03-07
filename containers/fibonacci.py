a29079169078bdfc2e2bb036de3d2f95c7c03de5
Nora Willett <leonora.willett.25@cmc.edu>
2023-03-06 22:08:09 -0800

    hw

--- containers/fibonacci.py
+++ containers/fibonacci.py
@@ -1,9 +1,4 @@
 
-################################################################################
-# example fibonacci number code;
-# you do not have to modify this code in any way
-################################################################################
-
 
 def fibs(n):
     '''
@@ -46,14 +41,8 @@ def fib(n):
     return f2
 
 
-################################################################################
-# fibonacci number code using generators;
-# you will need to implement the functions below
-################################################################################
-
-
 class Fib:
-    
+
     '''
     This class represents all the fibonacci numbers,
     but uses O(1) memory to do so.
@@ -63,66 +52,53 @@ class Fib:
     '''
     def __init__(self, n=None):
         self.n = n
-    
+
     def __repr__(self):
-        return f'Fib({self.n})'
+        if not self.n:
+            return 'Fib()'
+        else:
+            return f'Fib({self.n})'
 
     def __iter__(self):
         return FibIter(self.n)
+
 
 class FibIter:
     '''
     This is the iterator helper class for the Fib class.
     '''
 
-    def __init__(self,n):
+    def __init__(self, n):
         self.n = n
         self.result = 0
         self.i = 0
         self.prev = 0
         self.curr = 1
 
-
     def __next__(self):
-        #trying to figure out none type... do we want to go infinitely?
-        #if self.n != None:
-            
+        if self.n:
             if self.i >= self.n:
                 raise StopIteration
             else:
                 self.i += 1
-                if self.i <= 1 :
+                if self.i <= 1:
                     return 1
                 self.result = self.prev + self.curr
                 self.prev = self.curr
                 self.curr = self.result
                 return self.result
-
-    '''
         else:
-            if self.i > self.n:
-                raise StopIteration
-            else:
-                self.i = 0
-                while self.i:
-                    self.prev = 0
-                    self.curr = 0
-                    if i == 0:
-                        yield 1
-                    self.result = self.prev + self.curr
-                    self.prev = self.curr
-                    self.curr = self.result
-                    i += 1
-                    return self.result
-
-    '''
+            self.i += 1
+            if self.i <= 1:
+                return 1
+            self.result = self.prev + self.curr
+            self.prev = self.curr
+            self.curr = self.result
+            return self.result
 
 
 def fib_yield(n=None):
-    
-    
-    if n != None:
-
+    if n:
         if n < 2:
             yield 1
         prev = 0
@@ -147,8 +123,3 @@ def fib_yield(n=None):
             curr = next
             i += 1
             yield next
-
-    '''
-    This function returns a generator that computes the first n fibonacci numbers.
-    If n is None, then the generator is infinite.
-    '''
